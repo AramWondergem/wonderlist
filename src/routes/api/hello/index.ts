3daa32d2864235e177049859ba2086076fc07a4b
AramWondergem <aramwondergem@gmail.com>
2025-07-11 13:49:50 -0400

    finished styling, small start with database

--- src/routes/api/hello/index.ts
+++
@@ -1,8 +0,0 @@
-import { type RequestHandler } from '@builder.io/qwik-city';
-
-export const onGet: RequestHandler = async ({ json, request }) => {
-    console.log('📥 [SERVER] /api/hello endpoint aangeroepen');
-    console.log('🧾 [SERVER] Headers:', JSON.stringify([...request.headers]));
-
-    json(200, { message: 'Hallo van de server!' });
-};
